180e53c195e18fb0d80db01d132ff924527fb9e5
christopher-pedraza <62347713+christopher-pedraza@users.noreply.github.com>
2025-01-02 21:41:01 -0600

    Initial commit 🚀

---
+++ src/firebase-config.jsx
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCs3SRTiJPuF8Ca3TdvCyW8xKQ8HZO8AvU",
+    authDomain: "shareable-links-edd98.firebaseapp.com",
+    projectId: "shareable-links-edd98",
+    storageBucket: "shareable-links-edd98.firebasestorage.app",
+    messagingSenderId: "186092393905",
+    appId: "1:186092393905:web:9ba85cd67e3e5b4a9d9990",
+};
+
+const configuration = initializeApp(firebaseConfig);
+
+export default configuration;
