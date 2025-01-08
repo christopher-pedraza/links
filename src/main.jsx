0a75031ff9ffe1b6869c42c459b1922efd1667fe
christopher-pedraza <62347713+christopher-pedraza@users.noreply.github.com>
2025-01-08 11:11:00 -0600

    Install tailwind and nextui

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App.jsx";
+import "./index.css";
+import { NextUIProvider } from "@nextui-org/react";
 
 createRoot(document.getElementById("root")).render(
     <BrowserRouter>
-        <App />
+        <NextUIProvider>
+            <main className="dark text-foreground bg-background">
+                <App />
+            </main>
+        </NextUIProvider>
     </BrowserRouter>
 );
