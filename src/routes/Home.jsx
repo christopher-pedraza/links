dc8dee125019bad48d68f0d60fa732559e87e1de
christopher-pedraza <62347713+christopher-pedraza@users.noreply.github.com>
2025-01-10 13:13:00 -0600

    Form to add urls

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,40 @@
-import { Card } from "@nextui-org/react";
+import { Card, Form, Input, Button } from "@nextui-org/react";
+import { useState } from "react";
 
 export default function Home() {
+    const [submitted, setSubmitted] = useState(null);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        let formData = e.target.url.value;
+        // check if the url is valid and starts with http or https
+        if (!formData.match(/^(http|https):\/\//)) {
+            formData = "https://" + formData;
+        } else if (!formData) {
+            return;
+        }
+        console.log(formData);
+        setSubmitted(formData);
+    };
+
     return (
-        <Card className="p-4">
-            <h1 className="text-3xl">Welcome to the home page</h1>
-        </Card>
+        <div className="flex justify-center h-screen w-screen p-8">
+            <Card className="p-4 h-36 sm:h-24 w-3/4 flex justify-center items-center">
+                <Form
+                    className="w-full flex flex-col sm:flex-row items-center"
+                    onSubmit={onSubmit}
+                >
+                    <Input
+                        label="URL"
+                        labelPlacement="inside"
+                        name="url"
+                        placeholder="https://"
+                    />
+                    <Button size="lg" type="submit" variant="bordered">
+                        Add link
+                    </Button>
+                </Form>
+            </Card>
+        </div>
     );
 }
