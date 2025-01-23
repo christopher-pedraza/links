d995d92fdecbddfc6109cfddc6859efdda312da8
christopher-pedraza <62347713+christopher-pedraza@users.noreply.github.com>
2025-01-22 19:34:06 -0600

    Add name field to let users add a name to the link

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -6,7 +6,8 @@ import { useNavigate } from "react-router";
 
 export default function Home() {
     const [addedLinks, setAddedLinks] = useState([]);
-    const [value, setValue] = useState("");
+    const [name, setName] = useState("");
+    const [url, setURL] = useState("");
     const [isSharing, setIsSharing] = useState(false);
 
     const inputRef = useRef(null);
@@ -15,14 +16,14 @@ export default function Home() {
 
     const onSubmit = (e) => {
         e.preventDefault();
-        let formData = e.target.url.value;
-        if (!formData.match(/^(http|https):\/\//)) {
-            formData = "https://" + formData;
-        } else if (!formData) {
+        let urlValue = e.target.url.value;
+        if (!urlValue.match(/^(http|https):\/\//)) {
+            urlValue = "https://" + urlValue;
+        } else if (!urlValue) {
             return;
         }
-        setAddedLinks([...addedLinks, formData]);
-        setValue("");
+        setAddedLinks([...addedLinks, urlValue]);
+        setURL("");
         inputRef.current.focus();
     };
 
@@ -33,7 +34,7 @@ export default function Home() {
 
     const shareLinks = async () => {
         setIsSharing(true);
-        createDocument("links", "random name", addedLinks).then((id) => {
+        createDocument("links", name, addedLinks).then((id) => {
             if (!id) {
                 // TODO: Show error message to the user
                 setIsSharing(false);
@@ -43,49 +44,71 @@ export default function Home() {
         });
     };
 
+    const sanitizeName = (name) => {
+        // Sanitize name to be url friendly
+        setName(name.replace(/[^a-zA-Z0-9]/g, "").toLowerCase());
+    };
+
     return (
         <div className="flex items-center h-screen w-screen p-8 flex-col">
-            <Card className="p-4 h-36 sm:h-24 w-3/4 flex justify-center items-center mb-8">
-                <Form className="w-full" onSubmit={onSubmit}>
-                    <div className="w-full flex flex-col sm:flex-row items-center">
+            <Card className="p-4 h-52 sm:h-40 w-3/4 flex justify-center items-center mb-8">
+                <div className="w-full flex flex-col">
+                    <div className="w-full">
                         <Input
-                            ref={inputRef}
-                            label="URL"
+                            label="Link name (optional)"
                             labelPlacement="inside"
-                            name="url"
-                            placeholder="https://"
-                            size="lg"
-                            value={value}
-                            onValueChange={setValue}
+                            name="name"
+                            size="md"
+                            value={name}
+                            onValueChange={sanitizeName}
                         />
-                        <div className="flex flex-row ml-0 sm:ml-2 mt-2 sm:mt-0 justify-center items-center">
-                            <Button
-                                size="lg"
-                                type="submit"
-                                variant="bordered"
-                                className="mr-2"
-                            >
-                                Add link
-                            </Button>
-                            <Divider orientation="vertical" />
-                            <Button
-                                isIconOnly
-                                color="primary"
-                                onPress={shareLinks}
-                                isLoading={isSharing}
-                            >
-                                <ShareIcon />
-                            </Button>
-                        </div>
+                        <Divider
+                            orientation="horizontal"
+                            className="mt-2 mb-2"
+                        />
                     </div>
-                </Form>
+                    <Form className="w-full" onSubmit={onSubmit}>
+                        <div className="w-full flex flex-col sm:flex-row items-center">
+                            <Input
+                                ref={inputRef}
+                                label="URL"
+                                labelPlacement="inside"
+                                name="url"
+                                placeholder="https://"
+                                size="md"
+                                value={url}
+                                onValueChange={setURL}
+                            />
+                            <div className="flex flex-row ml-0 sm:ml-2 mt-2 sm:mt-0 justify-center items-center">
+                                <Button
+                                    size="md"
+                                    type="submit"
+                                    variant="bordered"
+                                    className="mr-2"
+                                >
+                                    Add link
+                                </Button>
+                                <Divider orientation="vertical" />
+                                <Button
+                                    size="md"
+                                    isIconOnly
+                                    color="primary"
+                                    onPress={shareLinks}
+                                    isLoading={isSharing}
+                                >
+                                    <ShareIcon />
+                                </Button>
+                            </div>
+                        </div>
+                    </Form>
+                </div>
             </Card>
             {addedLinks.length !== 0 && (
                 <Card className="p-2 pl-4 pr-4 w-3/4 flex items-center overflow-y-auto">
                     {addedLinks.map((link, index) => (
                         <Card
                             key={index}
-                            className="p-4 w-full flex items-center justify-between flex-row mb-2 mt-2 min-h-24"
+                            className="p-4 w-full flex items-center justify-between flex-row mb-2 mt-2 min-h-16"
                         >
                             <a
                                 href={link}
